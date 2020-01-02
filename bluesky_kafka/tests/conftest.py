273eef6731d98e053cba7af774edc4bb57e2b468
Joshua Lynch <jlynch@bnl.gov>
2020-01-02 16:49:21 -0500

    add command line option to specify bootstrap servers

--- bluesky_kafka/tests/conftest.py
+++ bluesky_kafka/tests/conftest.py
@@ -4,6 +4,22 @@ from bluesky.tests.conftest import RE
 import ophyd.sim
 
 
-@pytest.fixture(scope='function')
+# get bootstrap server IP from command line
+def pytest_addoption(parser):
+    parser.addoption(
+        "--kafka-bootstrap-servers",
+        action="store",
+        default="127.0.0.1:9092",
+        help="comma-separated list of address:port for Kafka bootstrap servers",
+    )
+
+
+@pytest.fixture
+def bootstrap_servers(request):
+    print(request)
+    return request.config.getoption("--kafka-bootstrap-servers")
+
+
+@pytest.fixture(scope="function")
 def hw(request):
     return ophyd.sim.hw()
