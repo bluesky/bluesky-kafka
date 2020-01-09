d6d2c0bfddd98842400a1a8533b396dbdb6c4aa1
Joshua Lynch <jlynch@bnl.gov>
2020-01-09 10:41:20 -0500

    identify topic and partition explicitly

--- bluesky_kafka/kafka.py
+++ bluesky_kafka/kafka.py
@@ -23,7 +23,7 @@ def delivery_report(err, msg):
     if err is not None:
         print("Message delivery failed: {}".format(err))
     else:
-        print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
+        print("Message delivered to topic ""{}"" [partition {}]".format(msg.topic(), msg.partition()))
 
 
 class Publisher:
