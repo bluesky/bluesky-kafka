0d5a3a92cc7039ca0490574413748c34c0b8ee06
Garrett Bischof <gbischof@bnl.gov>
2020-07-06 14:10:19 -0400

    Added LazyMongoRouter

--- bluesky_kafka/mongo_normalized_consumer.py
+++ bluesky_kafka/mongo_normalized_consumer.py
@@ -1,3 +1,4 @@
+import cachetools
 import msgpack
 import os
 
@@ -8,14 +9,40 @@ from suitcase.mongo_normalized import Serializer
 bootstrap_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
 mongo_uri = os.environ.get("BLUESKY_MONGO_URI")
 kafka_deserializer = partial(msgpack.loads, object_hook=mpn.decode)
-
-# "latest" should always work but has been failing on Linux, passing on OSX
-auto_offset_reset = "latest"
+auto_offset_reset = "latest"  # "latest" should always work but has been failing on Linux, passing on OSX
 topics = ["^*.bluesky.documents"]
 
-# Pass mongo_uri twice so that all documents go to the same database.
-# Hmm... I'm not sure how to make the documents go from a topic go to the right database.
-mongo_serializer = Serializer(mongo_uri, mongo_uri)
+
+class LazyMongoRouter():
+    """
+    Route messages to the correct mongo database, determined by the topic name.
+    TODO add caching mechanism to close old serializers.
+    """
+    def __init__(self, mongo_uri):
+        self._mongo_uri = mongo_uri
+        self._serializers = {}
+
+    @property
+    def serializers(self):
+        return self._serializers
+
+    def _get_serializer(self, topic):
+        database_name = topic.replace('.', '-')
+        serializer = Serializer(self._mongo_uri + database_name,
+                                self._mongo_uri + datamase_name)
+        self._serializers[topic] = serializer
+        return serializer
+
+    def __call__(self, topic, message):
+        try:
+            serializer = self._serialzers[topic]
+        except KeyError:
+            serializer = self._get_serializer(topic)
+        return serializer(*message)
+
+
+mongo_router = LazyMongoRouter(mongo_uri)
+
 
 kafka_dispatcher = RemoteDispatcher(
     topics=topics,
@@ -26,5 +53,7 @@ kafka_dispatcher = RemoteDispatcher(
     deserializer=kafka_deserializer,
 )
 
-kafka_dispatcher.subscribe(mongo_serializer)
+
+kafka_dispatcher.subscribe(mongo_router)
 kafka_dispatcher.start()
+
