f25e24b5828b811c1f61f484cb330448f6263874
Garrett Bischof <gbischof@bnl.gov>
2020-07-08 15:30:59 -0400

    update mongo_normalized_consumer

--- bluesky_kafka/mongo_normalized_consumer.py
+++ bluesky_kafka/mongo_normalized_consumer.py
@@ -2,7 +2,7 @@ import cachetools
 import msgpack
 import os
 
-from bluesky_kafka import RemoteDispatcher
+from bluesky_kafka import DynamicMongoConsumer
 from functools import partial
 
 bootstrap_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
@@ -11,17 +11,15 @@ kafka_deserializer = partial(msgpack.loads, object_hook=mpn.decode)
 auto_offset_reset = "latest"  # "latest" should always work but has been failing on Linux, passing on OSX
 topics = ["^*.bluesky.documents"]
 
-
-kafka_dispatcher = RemoteDispatcher(
+mongo_consumer = DynamicMongoConsumer(
     topics=topics,
     bootstrap_servers=bootstrap_servers,
     group_id="kafka-unit-test-group-id",
+    mongo_uri=mongo_uri,
     consumer_config={"auto.offset.reset": auto_offset_reset},
     polling_duration=1.0,
     deserializer=kafka_deserializer,
 )
 
-
-kafka_dispatcher.subscribe(mongo_router)
-kafka_dispatcher.start()
+mongo_consumer.start()
 
