6c5ca18ef233ea510a8abd87dcbdaa2c99e6a9a2
gbischof <gbischof@bnl.gov>
2020-07-30 14:28:50 -0400

    fix merge conflict, add exceptions if env var do not exist

--- bluesky_kafka/mongo_normalized_consumer.py
+++ bluesky_kafka/mongo_normalized_consumer.py
@@ -8,11 +8,18 @@ from bluesky_kafka import MongoConsumer
 
 
 bootstrap_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
+if bootstrap_servers is None:
+    raise AttributeError("Environment variable KAFKA_BOOTSTRAP_SERVERS"
+                         "must be set.")
+
 mongo_uri = os.environ.get("BLUESKY_MONGO_URI")
+if mongo_uri is None:
+    raise AttributeError("Environment variable BLUESKY_MONGO_URI "
+                         "must be set.")
+
 kafka_deserializer = partial(msgpack.loads, object_hook=mpn.decode)
 auto_offset_reset = "latest"
 topics = ["^.*bluesky.documents"]
-
 
 # Create a MongoConsumer that will automatically listen to new beamline topics.
 # The parameter metadata.max.age.ms determines how often the consumer will check for
