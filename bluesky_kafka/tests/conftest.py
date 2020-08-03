0f5641e9292ac11d22ee89346f6ac0507174801d
Garrett Bischof <gbischof@bnl.gov>
2020-08-03 12:50:18 -0400

    mongo_consumer (#7)
    * add script to start mongo_consumer
    
    * minor update
    
    * Added LazyMongoRouter
    
    * update RemoteDispatcher to pass the topic to subscribed funcitons
    
    * add DynamicMongoConsumer
    
    * add MongoSerialzerFactory
    
    * remove proccess function
    
    * removed LazyMongoRouter
    
    * update mongo_normalized_consumer
    
    * clean up the serializer after the stop doc is processed.
    
    * update to docstring
    
    * fix typo
    
    * add docstring for MongoSerializerFactory
    
    * add a /
    
    * undo changes to RemoteDispatcher
    
    * remove defaultdict import
    
    * update requirements
    
    * remove cachetools import
    
    * remove the Mongo specifics from DynamicMongoConsumer
    
    * update doc string
    
    * rename class
    
    * add post process to the factory class
    
    * rename some stuff
    
    * updates to DynamicMongoConsumer
    
    * more updates
    
    * minor fix
    
    * update docstring
    
    * update some names
    
    * More name changes
    
    * minor fix
    
    * docstring update
    
    * another docstring update
    
    * another docstring update
    
    * add a process arg to BlueskyConsumer
    
    * add process_document method
    
    * name change
    
    * add topic back into process_document apit
    
    * add test_mongo_consumer
    
    * add imports to test_mongo_consumer
    
    * add mongobox
    
    * add some pytest fixtures
    
    * several updates to the test
    
    * add methods to compare runs
    
    * add broker fixture
    
    * I think I fit all of the pieces together, haven't ran any code yet
    
    * update test doc string
    
    * got the code running, test is still failing
    
    * add test_bluesky_consumer
    
    * fixed the test by removing DocumentNames
    
    * found and fixed an error that was happening on the thread
    
    * got the consumer to read the documents
    
    * test is failing on the assertion now
    
    * IT PASSES!
    
    * remove some debug stuff
    
    * flake8 fixes
    
    * got the regex topics working
    
    * add a second publisher and entry to databroker catalog
    
    * added multi topic test, haven't ran the code yet
    
    * Multi topic test is passing
    
    * minor touchups
    
    * Update to docstring
    
    * add some more comments
    
    * remove yaml req
    
    * update databroker version
    
    * got the test passing with intake 0.6.0
    
    * review updates
    
    * more review updates
    
    * docstring update
    
    * more review updates
    
    * commit on stop doc
    
    * Update docstring
    
    * tests are passing locally
    
    * update travis.yml to try to fix osx tests
    
    * add pip list to travis.yml
    
    * add pyqt to requirements
    
    * add mongo service to travis
    
    * trying to debug CI
    
    * more debugging
    
    * trying to fix CI
    
    * only run mongo tests on linux
    
    * more CI debugging
    
    * xfail the multi client mongo test because it doesn't work on Travis
    
    * Change name of MongoBlueskyConsumer to MongoConsumer
    
    * Don't delete serializers
    
    * add auth source and fix commit_on_stop_doc

--- bluesky_kafka/tests/conftest.py
+++ bluesky_kafka/tests/conftest.py
@@ -1,7 +1,20 @@
+from functools import partial
+import os
+import yaml
+
+import intake
+import msgpack
+import msgpack_numpy as mpn
+import numpy as np
+import ophyd.sim
 import pytest
+import tempfile
 
 from bluesky.tests.conftest import RE
-import ophyd.sim
+from bluesky_kafka import Publisher
+
+TEST_TOPIC = "bluesky-kafka-test"
+TEST_TOPIC2 = "bluesky2-kafka-test"
 
 
 # get bootstrap server IP from command line
@@ -23,3 +36,112 @@ def bootstrap_servers(request):
 @pytest.fixture(scope="function")
 def hw(request):
     return ophyd.sim.hw()
+
+
+@pytest.fixture(scope="function")
+def msgpack_serializer(request):
+    return partial(msgpack.dumps, default=mpn.encode)
+
+
+@pytest.fixture(scope="function")
+def msgpack_deserializer(request):
+    return partial(msgpack.loads, object_hook=mpn.decode)
+
+
+@pytest.fixture(scope="function")
+def publisher(request, bootstrap_servers, msgpack_serializer):
+    return Publisher(
+        topic=TEST_TOPIC,
+        bootstrap_servers=bootstrap_servers,
+        key="kafka-unit-test-key",
+        # work with a single broker
+        producer_config={
+            "acks": 1,
+            "enable.idempotence": False,
+            "request.timeout.ms": 5000,
+        },
+        serializer=msgpack_serializer,
+        flush_on_stop_doc=True,
+    )
+
+
+@pytest.fixture(scope="function")
+def publisher2(request, bootstrap_servers, msgpack_serializer):
+    return Publisher(
+        topic=TEST_TOPIC2,
+        bootstrap_servers=bootstrap_servers,
+        key="kafka-unit-test-key",
+        # work with a single broker
+        producer_config={
+            "acks": 1,
+            "enable.idempotence": False,
+            "request.timeout.ms": 5000,
+        },
+        serializer=msgpack_serializer,
+        flush_on_stop_doc=True,
+    )
+
+
+@pytest.fixture(scope="function")
+def mongo_client(request):
+    mongobox = pytest.importorskip('mongobox')
+    box = mongobox.MongoBox()
+    box.start()
+    return box.client()
+
+
+@pytest.fixture(scope="function")
+def mongo_uri(request, mongo_client):
+    return f"mongodb://{mongo_client.address[0]}:{mongo_client.address[1]}"
+
+
+@pytest.fixture(scope="function")
+def numpy_md(request):
+    return {"numpy_data": {"nested": np.array([1, 2, 3])},
+            "numpy_scalar": np.float64(3),
+            "numpy_array": np.ones((3, 3))}
+
+
+@pytest.fixture(scope="function")
+def data_broker(request, mongo_uri):
+    TMP_DIR = tempfile.mkdtemp()
+    YAML_FILENAME = 'intake_test_catalog.yml'
+
+    fullname = os.path.join(TMP_DIR, YAML_FILENAME)
+
+    # Write a catalog file.
+    with open(fullname, 'w') as f:
+        f.write(f'''
+sources:
+  xyz:
+    description: Some imaginary beamline
+    driver: "bluesky-mongo-normalized-catalog"
+    container: catalog
+    args:
+      metadatastore_db: {mongo_uri}/{TEST_TOPIC}
+      asset_registry_db: {mongo_uri}/{TEST_TOPIC}
+      handler_registry:
+        NPY_SEQ: ophyd.sim.NumpySeqHandler
+    metadata:
+      beamline: "00-ID"
+  xyz2:
+    description: Some imaginary beamline
+    driver: "bluesky-mongo-normalized-catalog"
+    container: catalog
+    args:
+      metadatastore_db: {mongo_uri}/{TEST_TOPIC2}
+      asset_registry_db: {mongo_uri}/{TEST_TOPIC2}
+      handler_registry:
+        NPY_SEQ: ophyd.sim.NumpySeqHandler
+    metadata:
+      beamline: "00-ID"
+                ''')
+
+    def load_config(filename):
+        package_directory = os.path.dirname(os.path.abspath(__file__))
+        filename = os.path.join(package_directory, filename)
+        with open(filename) as f:
+            return yaml.load(f, Loader=getattr(yaml, 'FullLoader', yaml.Loader))
+
+    # Create a databroker with the catalog config file.
+    return intake.open_catalog(fullname)
